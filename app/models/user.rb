dbf88abd70f8109493b4fb9095c894d053cde294
foxtail1122 <160288948+foxtail1122@users.noreply.github.com>
2024-04-22 12:08:35 +0900

    before review2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,11 @@ class User < ApplicationRecord
 
 
         with_options presence: true do
-          validates :nickname, :birthday, :last_name, :first_name, :last_name_reading, :first_name_reading
-          validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "must be Full-width characters"}
-          validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "must be Full-width characters"}
-          validates :last_name_reading, format: {with: /\A[ァ-ヶー－]+\z/, message: "must be Kana Full-width katakana characters"}
-          validates :first_name_reading, format: {with: /\A[ァ-ヶー－]+\z/, message: "must be Kana Full-width katakana characters"}
-          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i , message: " is invalid. Include both letters and numbers" }
+          validates :nickname, :birthday
+          validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "must be Full-width characters", allow_blank: true}
+          validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "must be Full-width characters", allow_blank: true}
+          validates :last_name_reading, format: {with: /\A[ァ-ヶー－]+\z/, message: "must be Kana Full-width katakana characters", allow_blank: true}
+          validates :first_name_reading, format: {with: /\A[ァ-ヶー－]+\z/, message: "must be Kana Full-width katakana characters", allow_blank: true}
+          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i , message: " is invalid. Include both letters and numbers", allow_blank: true }
         end
-
-
-
 end
