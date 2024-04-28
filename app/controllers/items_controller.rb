a20e019e017935d7b9bed90980f8a366defb1004
foxtail1122 <160288948+foxtail1122@users.noreply.github.com>
2024-04-28 17:37:55 +0900

    before review

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-   # before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,8 +19,22 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    return if @item.user_id == current_user.id
+
+    redirect_to action: :index
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def show
-    @item = Item.find(params[:id])
   end
 
   private
@@ -30,7 +44,7 @@ class ItemsController < ApplicationController
                                  :day_id).merge(user_id: current_user.id)
   end
 
-  # def set_item
-  # @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
