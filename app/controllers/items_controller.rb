6eb7962d709ae58d176c90e60002fa7916bf9a43
foxtail1122 <160288948+foxtail1122@users.noreply.github.com>
2024-04-28 12:49:56 +0900

    before review

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  # before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -7,9 +7,9 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    return if user_signed_in?
+
+    redirect_to new_user_session_path
   end
 
   def create
@@ -21,9 +21,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
 
   private
+
   def item_params
-    params.require(:item).permit(:title, :image, :introduction, :category_id, :condition_id, :fee_id, :place_id, :price, :day_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :image, :introduction, :category_id, :condition_id, :fee_id, :place_id, :price,
+                                 :day_id).merge(user_id: current_user.id)
   end
+
+  # def set_item
+  # @item = Item.find(params[:id])
+  # end
 end
